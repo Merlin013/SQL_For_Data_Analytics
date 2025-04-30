fb53f96871b010fa82ec17961eca36905ef08794
Vishal Petkar <vishpetkar18@gmail.com>
2025-04-30 20:41:26 +0100

    Functions File created
    Concepts of different functions were learnt and various examples were practiced upon.

---
+++ Functions.sql
@@ -0,0 +1,21 @@
+"""Functions"""
+
+-- SUBSTRING function
+SELECT SUBSTRING('This is a Test String' FROM 11) test_data_extracted
+
+-- REPLACE function
+"Here we are replacing the word clothing with attire"
+SELECT department, 
+REPLACE(department, 'Clothing', 'Attire') modified_Data, 
+department || ' Department' new_department
+FROM departments
+
+-- POSITION function
+-- We can use the position function to help with extraction from a specific position
+
+SELECT POSITION('@' IN email)
+FROM employees
+
+-- Using this information we can extract the domain name in email. 
+SELECT SUBSTRING(email, POSITION('@' IN email) +1) as domain_name
+FROM employees
