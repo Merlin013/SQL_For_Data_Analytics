51a761eb61c3133c4bcf3d51d741b6417883b3b5
Vishal Petkar <vishpetkar18@gmail.com>
2025-05-24 06:34:19 +0100

    Update Grouping Functions.sql
    Completed learning about the different grouping functions

--- Grouping Functions.sql
+++ Grouping Functions.sql
@@ -7,3 +7,21 @@ FROM employees
 
 SELECT MAX(salary)
 FROM employees
+
+SELECT MIN(salary)
+FROM employees
+
+SELECT ROUND(AVG(salary))
+FROM employees
+
+SELECT COUNT(employee_id)
+FROM employees
+
+SELECT COUNT(email)
+FROM employees
+
+SELECT SUM(salary)
+FROM employees
+WHERE department = 'Clothing'
+
+
