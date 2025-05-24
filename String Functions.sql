ea584076d22f4a3a1773069863b52f46daf95217
Vishal Petkar <vishpetkar18@gmail.com>
2025-05-24 05:57:52 +0100

    Updated string function file
    Learnt different ways to use SUBSTRING(). Learnt REPLACE ()

--- String Functions.sql
+++ String Functions.sql
@@ -1,3 +1,15 @@
 SELECT 'This is test data' test_data
 
 SELECT SUBSTRING('This is test data' FROM 1 FOR 4) test_data_extracted
+
+SELECT SUBSTRING('This is test data' FROM 9 FOR 4) test_data_extracted
+
+SELECT SUBSTRING('This is test data' FROM 9) test_data_extracted
+
+SELECT department, REPLACE(department, 'Clothing', 'Attire') modified_data
+FROM departments
+
+SELECT department, 
+REPLACE(department, 'Clothing', 'Attire') modified_data, 
+(department || ' Department') as "Complete department name"
+FROM departments
